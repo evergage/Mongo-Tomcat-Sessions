780bc31a869105328726d62bfba9bd5f68048b84
Virinchi Nalluri <vnalluri@salesforce.com>
2022-03-30 11:15:13 -0400

    Changes for 8.5.76 - New Custom SessionIDGenerator

---
+++ src/main/java/com/dawsonsystems/session/MongoSessionIdGenerator.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2010-2022 salesforce.com, inc.
+ * All Rights Reserved. Company Confidential.
+ */
+
+package com.dawsonsystems.session;
+
+import org.apache.catalina.SessionIdGenerator;
+
+import java.util.UUID;
+
+/**
+ * From Tomcat 8.5.76 we have to use subclass of SessionIdGenerator with some overriding methods.
+ */
+public class MongoSessionIdGenerator implements SessionIdGenerator {
+
+    private String jvmRoute = "";
+
+    @Override
+    public String getJvmRoute() {
+        return this.jvmRoute;
+    }
+
+    @Override
+    public void setJvmRoute(String jvmRoute) {
+    }
+
+    @Override
+    public int getSessionIdLength() {
+        return 37;
+    }
+
+    @Override
+    public void setSessionIdLength(int sessionIdLength) {
+    }
+
+    @Override
+    public String generateSessionId() {
+        return UUID.randomUUID().toString();
+    }
+
+    @Override
+    public String generateSessionId(String route) {
+        return generateSessionId();
+    }
+}
